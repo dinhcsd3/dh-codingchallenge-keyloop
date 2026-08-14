4ab69119da7b7ef53aee1dd8af1ed366531f0883
dinhcsd3 <vandinh.hoang16@gmail.com>
2026-08-14 16:49:00 +0700

    date picker update

--- inventory-dashboard/src/app/app.config.ts
+++ inventory-dashboard/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 import { httpLoggingInterceptor } from './interceptors/http-logging.interceptor';
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([httpLoggingInterceptor]))
+    provideHttpClient(withInterceptors([httpLoggingInterceptor])),
+    provideNativeDateAdapter()
   ]
 };
